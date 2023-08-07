5971c0ecddab8ae1341b98260ad1062ff5644999
loitietu <1953442980@qq.com>
2023-08-07 19:49:33 +0800

    first commit

--- src/main/java/com/loistudio/file/MclStatic.java
+++ src/main/java/com/loistudio/file/MclStatic.java
@@ -1,8 +1,6 @@
 package com.loistudio.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -18,7 +16,7 @@ public class MclStatic {
         this.className = null;
     }
 
-    public void open(String path) throws FileNotFoundException {
+    public void open(String path) throws Exception {
         this.filename = path;
         File file = new File(this.filename);
         if (!file.exists()) {
@@ -27,7 +25,7 @@ public class MclStatic {
         }
     }
 
-    public void setConst(String constName, String value) throws FileNotFoundException {
+    public void setConst(String constName, String value) throws Exception {
         Map<String, Object> data = readData();
         if (!data.containsKey("consts")) {
             data.put("consts", new HashMap<String, Object>());
@@ -41,7 +39,7 @@ public class MclStatic {
         saveData(data);
     }
 
-    public void addClass(String className) throws FileNotFoundException {
+    public void addClass(String className) throws Exception {
         Map<String, Object> data = readData();
         if (!data.containsKey("classes")) {
             data.put("classes", new HashMap<String, Object>());
@@ -53,7 +51,7 @@ public class MclStatic {
         saveData(data);
     }
 
-    public void set(String className, String key, String value) throws FileNotFoundException {
+    public void set(String className, String key, String value) throws Exception {
         Map<String, Object> data = readData();
         if (!data.containsKey("classes")) {
             data.put("classes", new HashMap<String, Object>());
@@ -71,7 +69,7 @@ public class MclStatic {
         saveData(data);
     }
 
-    public String get(String className, String key) throws FileNotFoundException {
+    public String get(String className, String key) throws Exception {
         Map<String, Object> data = readData();
         if (data.containsKey("classes")) {
             Map<String, Object> classes = (Map<String, Object>)data.get("classes");
@@ -86,7 +84,7 @@ public class MclStatic {
         return null;
     }
 
-    public String getConst(String constName) throws FileNotFoundException {
+    public String getConst(String constName) throws Exception {
         Map<String, Object> data = readData();
         if (data.containsKey("consts")) {
             Map<String, Object> consts = (Map<String, Object>)data.get("consts");
@@ -98,7 +96,7 @@ public class MclStatic {
         return null;
     }
 
-    public void deleteClass(String className) throws FileNotFoundException {
+    public void deleteClass(String className) throws Exception {
         Map<String, Object> data = readData();
         if (data.containsKey("classes")) {
             Map<String, Object> classes = (Map<String, Object>)data.get("classes");
@@ -107,7 +105,7 @@ public class MclStatic {
         saveData(data);
     }
 
-    public void deleteKey(String className, String key) throws FileNotFoundException {
+    public void deleteKey(String className, String key) throws Exception {
         Map<String, Object> data = readData();
     
         if (data.containsKey("classes")) {
@@ -120,7 +118,7 @@ public class MclStatic {
         saveData(data);
     }  
 
-    public void deleteConst(String constName) throws FileNotFoundException {
+    public void deleteConst(String constName) throws Exception {
         Map<String, Object> data = readData();
         if (data.containsKey("consts")) {
             Map<String, Object> consts = (Map<String, Object>)data.get("consts");
@@ -129,7 +127,7 @@ public class MclStatic {
         saveData(data);
     }
 
-    public void saveData(Map<String, Object> data) throws FileNotFoundException {
+    public void saveData(Map<String, Object> data) throws Exception {
         StringBuilder output = new StringBuilder();
         if (data.containsKey("consts")) {
             Map<String, Object> consts = (Map<String, Object>)data.get("consts");
@@ -150,18 +148,20 @@ public class MclStatic {
                 output.append("}\n");
             }
         }
-        PrintWriter writer = new PrintWriter(new File(this.filename));
-        writer.write(output.toString().getBytes().toString());
-        writer.close();
+        DataOutputStream out = new DataOutputStream(new FileOutputStream(this.filename));
+        out.writeUTF(toHex(output.toString()));
+        out.close();
     }
 
-    public Map<String, Object> readData() throws FileNotFoundException {
+    public Map<String, Object> readData() throws Exception {
         Map<String, Object> data = new HashMap<>();
         data.put("consts", new HashMap<String, Object>());
         data.put("classes", new HashMap<String, Object>());
         File file = new File(this.filename);
-        if (file.exists()) {
-            Scanner scanner = new Scanner(file); 
+        if (file.exists() && file.length() > 0) {
+            DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(this.filename)));
+            Scanner scanner = new Scanner(hexToStr(in.readUTF()));
+            in.close();
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.startsWith("CONST")) {
@@ -173,15 +173,18 @@ public class MclStatic {
                     ((Map<String, Object>)data.get("consts")).put(constName, constItem);
                 } else if (line.startsWith("CLASS")) {
                     this.className = line.split(" ")[1];
+                    Map<String,Object> classItem = new HashMap<>();  
+                    ((Map<String,Object>)data.get("classes")).put(className, classItem);
                 } else if (line.startsWith("    KEY")) {
                     String[] parts = line.split(" ");
-                    String key = parts[1];
-                    String value = parts[3];
+                    String key = parts[5];
+                    String value = parts[7];
                     Map<String, Object> keyItem = new HashMap<>();
                     keyItem.put("value", value);
                     Map<String, Object> classData = (Map<String, Object>)data.get("classes");
                     Map<String, Object> classItem = (Map<String, Object>)classData.get(className);
                     classItem.put(key, keyItem);
+                    ((Map<String,Object>)data.get("classes")).put(className, classItem);
                 }
             }
             scanner.close();
@@ -190,7 +193,7 @@ public class MclStatic {
     }
 
     public String generateMixed(int n) {
-        String chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+        String chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz123456789=";
         StringBuilder sb = new StringBuilder(n);
         Random random = new Random(System.nanoTime());
         for(int i = 0; i < n; i++) {
@@ -198,5 +201,31 @@ public class MclStatic {
         }
         return sb.toString();
     }
-
+    
+    public String toHex(String str) {
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        byte[] bs = str.getBytes();
+        int bit;
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            sb.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            sb.append(chars[bit]);
+        }
+        return sb.toString().trim();
+    }
+    
+    public String hexToStr(String hexStr) {
+        String str = "0123456789ABCDEF";
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            n = str.indexOf(hexs[2 * i]) * 16;
+            n += str.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes);
+    }
 }

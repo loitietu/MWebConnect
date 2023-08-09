9d6a2a9c34881bfc0002fdf89a2b65c0e947b1aa
loitietu <1953442980@qq.com>
2023-08-10 00:27:37 +0800

    first commit

--- src/main/java/com/loistudio/file/MclStatic.java
+++ src/main/java/com/loistudio/file/MclStatic.java
@@ -9,21 +9,28 @@ import java.util.Base64;
 import java.math.BigInteger;
 
 import com.loistudio.tools.AseEncrypt;
+import com.loistudio.tools.DigitalSignature;
 
 public class MclStatic {
 
     private String filename;
     private String className;
     private String key;
+    private String publicPath;
+    private String privatePath;
 
     public MclStatic() {
         this.filename = null;
         this.className = null;
         this.key = "LOIStudio";
+        this.publicPath = "public.key";
+        this.privatePath = "private.key";
     }
     
-    public void setKey(String key) {
+    public void setKey(String key, String publicPath, String privatePath) {
         this.key = key;
+        this.publicPath = publicPath;
+        this.privatePath = privatePath;
     }
     
     public String getKey() {
@@ -162,8 +169,10 @@ public class MclStatic {
                 output.append("}\n");
             }
         }
+        BigInteger file = base64ToDecimal(AseEncrypt.encryptASE192(AseEncrypt.encryptTripleDES(AseEncrypt.encryptASE192(toHex(output.toString()), this.key), this.key), this.key));
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(this.filename));
-        out.writeObject(base64ToDecimal(AseEncrypt.encrypt(toHex(output.toString()), this.key)));
+        out.writeObject(DigitalSignature.signData(output.toString(), this.privatePath));
+        out.writeObject(file);
         out.close();
     }
 
@@ -174,34 +183,40 @@ public class MclStatic {
         File file = new File(this.filename);
         if (file.exists() && file.length() > 0) {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream(this.filename));
-            Scanner scanner = new Scanner(hexToStr(AseEncrypt.decrypt(decimalToBase64((BigInteger) in.readObject()), this.key)));
-            in.close();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.startsWith("CONST")) {
-                    String[] parts = line.split(" ");
-                    String constName = parts[1];
-                    String value = parts[3];
-                    Map<String, Object> constItem = new HashMap<>();
-                    constItem.put("value", value);
-                    ((Map<String, Object>)data.get("consts")).put(constName, constItem);
-                } else if (line.startsWith("CLASS")) {
-                    this.className = line.split(" ")[1];
-                    Map<String,Object> classItem = new HashMap<>();  
-                    ((Map<String,Object>)data.get("classes")).put(className, classItem);
-                } else if (line.startsWith("    KEY")) {
-                    String[] parts = line.split(" ");
-                    String key = parts[5];
-                    String value = parts[7];
-                    Map<String, Object> keyItem = new HashMap<>();
-                    keyItem.put("value", value);
-                    Map<String, Object> classData = (Map<String, Object>)data.get("classes");
-                    Map<String, Object> classItem = (Map<String, Object>)classData.get(className);
-                    classItem.put(key, keyItem);
-                    ((Map<String,Object>)data.get("classes")).put(className, classItem);
+            byte[] singdata = (byte[]) in.readObject();
+            String files = hexToStr(AseEncrypt.decryptASE192(AseEncrypt.decryptTripleDES(AseEncrypt.decryptASE192(decimalToBase64((BigInteger) in.readObject()), this.key), this.key), this.key));
+            if (DigitalSignature.verifySignature(files, singdata, this.publicPath)) {
+                Scanner scanner = new Scanner(files);
+                in.close();
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.startsWith("CONST")) {
+                        String[] parts = line.split(" ");
+                        String constName = parts[1];
+                        String value = parts[3];
+                        Map<String, Object> constItem = new HashMap<>();
+                        constItem.put("value", value);
+                        ((Map<String, Object>)data.get("consts")).put(constName, constItem);
+                    } else if (line.startsWith("CLASS")) {
+                        this.className = line.split(" ")[1];
+                        Map<String,Object> classItem = new HashMap<>();  
+                        ((Map<String,Object>)data.get("classes")).put(className, classItem);
+                    } else if (line.startsWith("    KEY")) {
+                        String[] parts = line.split(" ");
+                        String key = parts[5];
+                        String value = parts[7];
+                        Map<String, Object> keyItem = new HashMap<>();
+                        keyItem.put("value", value);
+                        Map<String, Object> classData = (Map<String, Object>)data.get("classes");
+                        Map<String, Object> classItem = (Map<String, Object>)classData.get(className);
+                        classItem.put(key, keyItem);
+                        ((Map<String,Object>)data.get("classes")).put(className, classItem);
+                    }
                 }
+                scanner.close();
+            } else {
+                throw new DigitalSignatureException("Inconsistent digital signatures.");
             }
-            scanner.close();
         }
         return data;
     }
@@ -263,5 +278,11 @@ public class MclStatic {
         BigInteger decimal = new BigInteger(decimals);
         String hex = decimal.toString(16);
         return hex;
+    }
+}
+
+class DigitalSignatureException extends RuntimeException {
+    public DigitalSignatureException(String message) {
+        super(message);
     }
 }
--- src/main/java/com/loistudio/tools/AseEncrypt.java
+++ src/main/java/com/loistudio/tools/AseEncrypt.java
@@ -6,9 +6,11 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class AseEncrypt {
-    public static String encrypt(String input, String key) throws Exception {
+    public static String encryptASE192(String input, String key) throws Exception {
         byte[] keyBytes = generateKey(key);
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
@@ -19,7 +21,7 @@ public class AseEncrypt {
         return Base64.getEncoder().encodeToString(encryptedBytes);   
     }
     
-    public static String decrypt(String input, String key) throws Exception {
+    public static String decryptASE192(String input, String key) throws Exception {
         byte[] keyBytes = generateKey(key);  
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");   
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
@@ -30,6 +32,53 @@ public class AseEncrypt {
         byte[] decrypted = cipher.doFinal(decryptedBytes);
         return new String(decrypted);
     }
+    
+    public static String encryptDES(String data, String key) throws Exception {
+        byte[] keyBytes = generateKey(key);
+        byte[] desKey = new byte[8];
+        System.arraycopy(keyBytes, 0, desKey, 0, 8);
+        SecretKey secretKey = new SecretKeySpec(desKey, "DES");
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptData = cipher.doFinal(data.getBytes());
+        String base64 = Base64.getEncoder().encodeToString(encryptData);
+        return base64;   
+    }
+        
+    public static String decryptDES(String data, String key) throws Exception{
+        byte[] keyBytes = generateKey(key);
+        byte[] desKey = new byte[8];
+        System.arraycopy(keyBytes, 0, desKey, 0, 8);
+        SecretKey secretKey = new SecretKeySpec(desKey, "DES");
+        Cipher cipher = Cipher.getInstance("DES");     
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] base64Data = Base64.getDecoder().decode(data);
+        byte[] decryptData = cipher.doFinal(base64Data);    
+        return new String(decryptData);
+    }
+    
+    public static String encryptTripleDES(String input, String key) throws Exception {
+        byte[] keyBytes = generateKey(key);
+        byte[] desKey = new byte[24];
+        System.arraycopy(keyBytes, 0, desKey, 0, 24);
+        SecretKey secretKey = new SecretKeySpec(desKey, "DESede");
+        Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+    
+    public static String decryptTripleDES(String input, String key) throws Exception {
+        byte[] keyBytes = generateKey(key);
+        byte[] desKey = new byte[24];
+        System.arraycopy(keyBytes, 0, desKey, 0, 24);
+        SecretKey secretKey = new SecretKeySpec(desKey, "DESede");
+        Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = Base64.getDecoder().decode(input);
+        byte[] decrypted = cipher.doFinal(decryptedBytes);
+        return new String(decrypted);
+    } 
     
     private static byte[] generateKey(String key) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");  
